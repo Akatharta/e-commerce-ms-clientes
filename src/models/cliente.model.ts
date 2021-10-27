850b5aafe646a124fd391980efaee0bac01de8b1
Jessica Ramirez <ramzgonz.j@gmail.com>
2021-10-27 19:06:01 -0400

    Datasource, modelos, repositorios, relaciones y controladores

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property, hasOne} from '@loopback/repository';
+import {DireccionCliente} from './direccion-cliente.model';
+import {UsuarioCliente} from './usuario-cliente.model';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  nombre?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'number',
+    default: 1,
+  })
+  estado?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  documento: string;
+
+  @hasOne(() => DireccionCliente, {keyTo: 'id_cliente'})
+  direccionCliente: DireccionCliente;
+
+  @hasOne(() => UsuarioCliente, {keyTo: 'id_cliente'})
+  usuarioCliente: UsuarioCliente;
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
